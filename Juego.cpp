7a0170e5cad83f93a794d2497d062ad008872b66
Joaquin Rendon <69638517+ParadoxToFu@users.noreply.github.com>
2023-11-17 10:09:03 -0500

    Update Juego.cpp

--- Juego.cpp
+++ Juego.cpp
@@ -4,16 +4,16 @@ int xPosition = 0;
 int yPosition = 0;
 
 int i; //recorre el array de los leds para inicio
-int j;  //# de leds marcados
+int j;  //leds a marcar
 signed int k;  //recorre el array de leds
 int presionado=0;
-int suerte=1;
-int selec[marcas];
+int selec[marcas]={33,33,33,33};
+int suerte;
 
-CRGB azu=(0,0,255);//colores: axul, naranja, rojo, leds apagados
-CRGB nar=(288,128,0);
-CRGB roj=(255,0,0);
-CRGB apa=(0,0,0);
+CRGB azu=CRGB(0,0,255);//colores: axul, naranja, rojo, leds apagados
+CRGB nar=CRGB(255,128,0);
+CRGB roj=CRGB(255,0,0);
+CRGB apa=CRGB(0,0,0);
 CRGB leds[NUM_LEDS];
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
@@ -21,80 +21,113 @@ void IRAM_ATTR button(){ //Interrupcion boton joystic
 presionado=1;
 }
 
-void inicio(){//enciende todos los leds
-  i=0;
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Inicio del juego");
+void inicio()//enciende todos los leds
+{
+  int i=0;
     for (i = 0; i< NUM_LEDS; i++){
         leds[i] = azu;
-        delay(80);
+        FastLED.show();
+        delay(50);
+    }
+     for (i = 0; i< NUM_LEDS; i++){
+        leds[i] =nar;
+        FastLED.show();
+        delay(50);
+    }
+    for (i = 0; i< NUM_LEDS; i++){
+        leds[i] = azu;
+        FastLED.show();
+        delay(50);
     }
 }
 
 void seleccion(){
-  int rest=marcas-j;
+  int rest=marcas;
+  presionado=0;
   j=0;//# de leds marcados
   k=0;//posision el array de los leds
+  leds[k]=roj;
+  FastLED.show();
+  int l=0;
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Selecciones Restantes:");
-  lcd.setCursor(8,1);
-  lcd.print(rest);
-  while (j<marcas+1){
-    xPosition = analogRead(xPin);
+  lcd.print("Posiciones Restantes");
+  delay(1000);
+  printf("Numeros Seleccionados=\n");
+  while (j<marcas){
+    l=k;
+    lcd.setCursor(9,2);
+    lcd.print(rest);
     yPosition = analogRead(yPin);
-    if (k!=selec[0]||k!=selec[1]||k!=selec[2]||k!=selec[3]||k!=selec[4]){//Parpadeo de leds mientras el "cursor esta encima de ellos"
+    //Serial.print(yPosition);
+    if (yPosition>=3500){//joystic hacia abajo
+      k++;
+      if (k>=NUM_LEDS){//mayor igual
+        k=0;
+        }
+      leds[l]=azu;
       leds[k]=roj;
-      delay(50);
-      leds[k]=azu;
-      delay(50);
-    }
-    if ((presionado==1)&&k!=selec[0]||k!=selec[1]||k!=selec[2]||k!=selec[3]||k!=selec[4])//se añade el led por su posicion a la lista de los seleccionados por el usuario
+      FastLED.show();
+      //printf("k%i\n",k);
+      delay(100);
+      }
+    if (yPosition<=2000){//joystic hacia arriba
+      k--;
+      if (k<=-1){//menor igual
+        k=NUM_LEDS-1;
+        }
+      leds[l]=azu;
+      leds[k]=roj;
+      FastLED.show();
+      //printf("k%i\n",k);
+      delay(100);
+      }
+    if (presionado==1)//se añade el led por su posicion a la lista de los seleccionados por el usuario
     {
       selec[j]=k;
+      printf("%i ",selec[j]);
       leds[k]=roj;
+      FastLED.show();
       j++;
       rest=marcas-j;
+      delay(100);
       presionado=0;
-      lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print("Selecciones Restantes:");
-      lcd.setCursor(8,1);
-      delay(200);
-      lcd.print(rest);
      }
-    if (xPosition>=4000 || yPosition>=4000){k++;delay(150);}//joystic hacia la derecha o abajo = movimiento a la derecha
-    if (xPosition<=2000 || yPosition<=2000){k--;delay(150);}//joystic hacia la izquierda o arriba = movimiento a la izquierda
-    if (k<=-1){k=NUM_LEDS;delay(150);}//menor igual
-    if (k>=NUM_LEDS+1){k=0;delay(150);}//mayor igual
-    else {;};
+    for(int i=0;i<marcas;i++){
+      leds[selec[i]] = roj;
+      FastLED.show();
+    }
   }
+  printf("\n");
+  lcd.setCursor(9,2);
+  lcd.print(rest);
+  delay(2000);
+}
+
+void pazu(int a[], CRGB b[]){
+for (i = 0; i< random; i++)
+    {
+        b[a[i]] = azu;
+        FastLED.show();
+        delay(50);
+    }
+}
+
+void pnar(int a[], CRGB b[]){
+for (i = 0; i< random; i++)
+    {
+        b[a[i]] = nar;
+        FastLED.show();
+        delay(50);
+    }
 }
 
 void recorrido(int a[], CRGB b[]){//efecto de luz led
       lcd.clear();
       lcd.setCursor(0,0);
-      lcd.print("¡Cruza los dedos!");
-    for (i = 0; i< random; i++)
-    {
-        b[a[i]] = nar;
-        delay(100);
-        b[a[i]] = azu;
-        delay(100);
-    }
-    for (i = 0; i< random; i++)
-    {
-        b[a[i]] = nar;
-        delay(100);
-        b[a[i]] = azu;
-        delay(100);
-    }
-    for (i = 0; i< random; i++)
-    {
-        b[a[i]] = nar;
-        delay(100);
-    }
+      lcd.print("Cruza los dedos!");
+      pazu(a,b);
+      pnar(a,b);
 }
 
 void comprobacion(int a[], int b[], int c, int d)//array leds ganandores, array de selecionados, #leds ganadores, #leds seleccionados 
@@ -114,29 +147,100 @@ if (suerte!=1){suerte=0;}// si no tuvo ni un acierto, perdio
 
 void gameover(){
   lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Mala Suerte");
-  delay(600);
-  lcd.setCursor(0,1);
-  lcd.print("Fin del Juego");
+  lcd.setCursor(4,1);
+  lcd.print("MALA SUERTE");
+  delay(2000);
+  lcd.clear();
+  lcd.setCursor(3,1);
+  lcd.print("FIN DEL JUEGO");
+  delay(2000);
 }
 
-int play()
-{
-while (suerte==1)// preguntar si quiere volver a jugar, con un boton de interrupcion??
-{
-inicio();
-seleccion();
-int arr[random];
-srand(time(NULL));
-for (int i=0;i<random;i++)
+int play() {
+  inicio();
+  for(int i=0;i<marcas;i++){
+      selec[i] = 30;
+      FastLED.show();
+    }
+  seleccion();
+  int arr[random];
+  srand(time(NULL));
+  printf("Numeros Ganadores=\n");
+  for (int i=0;i<random;i++)
+    {
+      arr[i]=1+rand()%(0-29);
+      printf("%i ",arr[i]);
+    }
+  printf("\n");
+  recorrido (arr,leds);
+  delay(1000);
+  recorrido (arr,leds);
+  delay(1000);
+  recorrido (arr,leds);
+  delay(1000);
+  pnar(arr,leds);
+  comprobacion(arr, selec, random, marcas);
+  delay(2000);
+  if(suerte==1){         
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("GANASTE UN INTENTO!");
+  delay(3000);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("JUGAR DENUEVO?");
+  delay(3000);
+  presionado=0;
+  lcd.setCursor(0,1);
+  lcd.print(">");
+  lcd.setCursor(1,1);
+  lcd.print(" SI ");
+  lcd.setCursor(1,2);
+  lcd.print(" NO");
+  while(presionado==0)      
   {
-    arr[i]=1+rand()%(0-30);
-  }
-recorrido (arr,leds);
-comprobacion(arr, selec, random, marcas);
-return suerte;
-}
-gameover();
-return suerte;
+    xPosition = analogRead(xPin);
+    yPosition = analogRead(yPin);
+    if (yPosition>=3000){//joystic hacia arriba
+      i=0;
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("JUGAR DENUEVO?");
+      lcd.setCursor(0,1);
+      lcd.print(">");
+      lcd.setCursor(1,1);
+      lcd.print(" SI ");
+      lcd.setCursor(1,2);
+      lcd.print(" NO");
+      delay(100);
+    }
+    if (yPosition<=1000){//joystic hacia abajo
+      i=1;
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("JUGAR DENUEVO?");
+      lcd.setCursor(0,2);
+      lcd.print(">");
+      lcd.setCursor(1,1);
+      lcd.print(" SI ");
+      lcd.setCursor(1,2);
+      lcd.print(" NO");
+      delay(100);
+    }
+    //else{;};
+    }
+    if (i==1){
+      lcd.clear();
+      lcd.setCursor(7,0);
+      lcd.print("ADIOS");
+      delay(2000);
+      suerte=0;
+      return suerte;
+     }
+    if (i==0)
+    {
+      return suerte;
+    }
+    printf("\n%i\n",suerte);
+    }
 }
